53a17b340bdedc618ea5cd2333af36554053ed71
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-05-25 00:18:36 +0900

    네비게이션 뷰 2개로 줄이고, 설정창 완료 및 배경 통일 완료

---
+++ offender_location8/app/src/main/java/com/example/offender_location5/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.offender_location5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reactivity_sign_up);
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+    }
+}
