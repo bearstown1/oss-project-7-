ed435997408290218ff01f3a5d734417c01a6d78
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-05-25 01:01:06 +0900

    (세팅창)GUI 위치지정 다시한것

---
+++ offender_location9/app/src/main/java/com/example/offender_location5/NotificationFragment.java
@@ -0,0 +1,18 @@
+package com.example.offender_location5;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notification, null);
+    }
+}
