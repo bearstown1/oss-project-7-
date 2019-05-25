e5defd78ba5ec7d1c8198bf183d07c18b840b3c0
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-05-25 20:03:04 +0900

    오류 수정 및 보완

---
+++ offender_location9.5/app/src/main/java/com/example/offender_location5/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.offender_location5;
+
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+               button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+
+
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == 101){
+//            String name = data.getStringExtra("name");
+//            Toast.makeText(getApplicationContext(),"로그인화면으로 이동합니다" + name,Toast.LENGTH_LONG).show();
+//        }
+//    }
+}
