a7c39a455d1fb3059daccac293c3abfe70435386
sjhh0712 <sjhh0712@naver.com>
2019-05-29 00:43:27 +0900

    offender_location9.5(파이어베이스 연동 버전)

--- offender_location9.5/app/src/main/java/com/example/offender_location5/SignUpActivity.java
+++ offender_location9.5/app/src/main/java/com/example/offender_location5/SignUpActivity.java
@@ -1,18 +1,33 @@
 package com.example.offender_location5;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private  static final String TAG  = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reactivity_sign_up);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         Button button3 = (Button)findViewById(R.id.button4);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +36,46 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivityForResult(intent, 101);
             }
         });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.button4:
+
+                    break;
+            }
+        }
+    };
+
+    private  void signUp(){
+        String email = ((EditText)findViewById(R.id.editText3)).getText().toString();
+        String password = ((EditText)findViewById(R.id.editText4)).getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //UI
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //UI
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
